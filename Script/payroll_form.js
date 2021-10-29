d8f0c90f3033d18db451cf994271d3ca74f3b3b6
ka1peshc <kalpesh111997@gmail.com>
2021-10-29 17:20:08 +0530

    Add: check user enter date with current date

--- Script/payroll_form.js
+++ Script/payroll_form.js
@@ -22,4 +22,19 @@ window.addEventListener('DOMContentLoaded',(event)=>{
     salary.addEventListener('input',function(){
         output.textContent=salary.value;
     });
+
+    //Date check
+    
+    const day = document.querySelector('#day').value;
+    const month = document.querySelector('#month').value;
+    const year = document.querySelector('#year').value;
+    let currDate = new Date();
+    let user_date = new Date(year+"-"+month+"-"+day); 
+    const date_error = document.querySelector('.date-error');
+    year.addEventListener('input',function(){
+        if(user_date > currDate){
+            date_error.textContent="You cannot select future Date"
+        }
+    })
+
 });
