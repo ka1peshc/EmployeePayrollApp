1f5395bf319f3202a18546d51831c039022fc4d9
ka1peshc <kalpesh111997@gmail.com>
2021-10-29 11:34:11 +0530

    Add: validation to raidobutton and regex for name

--- Script/Events.js
+++ Script/Events.js
@@ -4,3 +4,23 @@ const displaySal = document.querySelector('#displaySalary');
 sal.addEventListener('input',function (){
     displaySal.textContent= document.getElementById("salary").value;
 })
+
+const userName = document.querySelector('#name');
+const nameError = document.querySelector('#displayNameError');
+userName.addEventListener('input',function(){
+    let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+    if(nameRegex.test(userName.value)){
+        nameError.textContent="";
+    }else{
+        nameError.textContent="Name is Incorrect";
+    }
+});
+function CheckAllField(){
+    const checkValue = document.querySelector('.checkbox').value;
+
+    const day = document.querySelector('#day');
+    const month = document.querySelector('#month');
+    const year = document.querySelector('#year');
+
+       alert(checkValue);
+}
